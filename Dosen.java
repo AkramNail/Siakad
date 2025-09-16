3018f71887a8c2b2e2dbd275b579f3864c38867f
AkramNail <udinakram1000@Gmail.com>
2025-09-16 19:10:01 +0700

    Memperbaiki file dosen

--- Dosen.java
+++ Dosen.java
@@ -1,4 +1,19 @@
 public class Dosen {
 
     String namaDosen;
+    int NIP;
+
+
+    public void memberiNilai() {
+        
+        
+
+    }
+
+    public void mengapsenMahasiswa() {
+        
+
+
+    }
+    
 }
